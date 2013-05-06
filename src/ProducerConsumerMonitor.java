2181d06d69165e9f17ad5075255e8874462d71ba
erangeljr <hacker@eddierangel.com>
2013-05-05 21:47:34 -0700

    Added Contents to Monitor
    Creating Put and Get Synchronized methods in PCMonitor

--- src/ProducerConsumerMonitor.java
+++ src/ProducerConsumerMonitor.java
@@ -1,4 +1,16 @@
 
 public class ProducerConsumerMonitor {
 
+	int label = 1;
+	int contain[] = new int [10], in = 0, out = 0, count = 0;
+
+	public synchronized int put(){
+
+		return 0;
+	}
+
+	public synchronized int get(){
+
+		return -1;
+	}
 }
