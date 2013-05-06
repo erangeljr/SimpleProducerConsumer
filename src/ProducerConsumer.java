9937a15675500a3a8ae8acc42f5d3167b8b4b85f
erangeljr <hacker@eddierangel.com>
2013-05-05 21:37:33 -0700

    Created Class
    Added ProducerConsumerMonitor class

---
+++ src/ProducerConsumer.java
@@ -0,0 +1,16 @@
+
+public class ProducerConsumer {
+	
+	public static void main(String[] args){
+	
+		ProducerConsumerMonitor pcMonitor = new ProducerConsumerMonitor();
+		
+//		for(int k = 1; k < 3; k++)
+//			(new Thread(new Producer (k, pcMonitor))).start();	
+//
+//		for(int k = 1; k < 4; k++)
+//			(new Thread(new Consumer (k, pcMonitor))).start();
+	}
+
+	
+}
