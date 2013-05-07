491e31eb1f7956dd8c3c353ad246d9e921683071
erangeljr <hacker@eddierangel.com>
2013-05-06 21:15:12 -0700

    Refactoring Code
    Moving Classes into ProducerConsumer.Java.
    Only performing this task for demonstration purposes. This would
    normally not be done for modularity.

--- src/ProducerConsumer.java
+++ src/ProducerConsumer.java
@@ -14,3 +14,55 @@ public class ProducerConsumer {
 
 	
 }
+
+class ProducerConsumerMonitor {
+
+	int label = 1;
+	int contain[] = new int [10], in = 0, out = 0, count = 0;
+
+	public synchronized int put(){
+
+		return 0;
+	}
+
+	public synchronized int get(){
+
+		return -1;
+	}
+}
+
+class Producer implements Runnable {
+	int ID; ProducerConsumerMonitor mtr;
+	public Producer(int id, ProducerConsumerMonitor pcMonitor){
+		ID = id; this.mtr = pcMonitor;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.printf("Producer %d is making an item.\n", ID);
+		try{}
+		catch(Exception e){}
+
+	}
+
+}
+
+class Consumer implements Runnable {
+	
+	int ID; ProducerConsumerMonitor mtr;
+	
+	public Consumer(int id, ProducerConsumerMonitor pcMonitor){
+		ID = id; this.mtr = pcMonitor;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.printf("Consumer %d is making an item.\n", ID);
+		try{}
+		catch(Exception e){}
+
+	}
+
+}
